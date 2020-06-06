8775c96254d86b6e1a149b7f29432a8e7d22afe2
shoter <lubiebardzozelki@gmail.com>
2020-06-06 22:23:51 +0200

    Day now lasts 1 day lol

--- src/date_type.h
+++ src/date_type.h
@@ -28,9 +28,9 @@ typedef uint8  Day;   ///< Type for the day of the month, note: 1 based, first d
  * 1 tick is approximately 30 ms.
  * 1 day is thus about 2 seconds (74 * 30 = 2220) on a machine that can run OpenTTD normally
  */
-static const int MINUTE_TICKS = 1980; /// It's not exact this value but this is very good aproximation.
-static const int HOUR_TICKS = 118800;
-static const int DAY_TICKS         = 2851200; ///< ticks per day
+static const int MINUTE_TICKS = 2000; /// It's not exact this value but this is very good aproximation.
+static const int HOUR_TICKS = 120000;
+static const int DAY_TICKS         = 2880000; ///< ticks per day
 static const int DAYS_IN_YEAR      = 365; ///< days per year
 static const int DAYS_IN_LEAP_YEAR = 366; ///< sometimes, you need one day more...
 
@@ -39,6 +39,7 @@ static const int STATION_ACCEPTANCE_TICKS = 250; ///< cycle duration for updatin
 static const int STATION_LINKGRAPH_TICKS  = 504; ///< cycle duration for cleaning dead links
 static const int CARGO_AGING_TICKS        = HOUR_TICKS; ///< cycle duration for aging cargo
 static const int INDUSTRY_PRODUCE_TICKS   = HOUR_TICKS; ///< cycle duration for industry production
+static const int TOWN_PRODUCE_TICKS		  = MINUTE_TICKS * 30; ///< cycle duration for industry production
 static const int TOWN_GROWTH_TICKS        = HOUR_TICKS;  ///< cycle duration for towns trying to grow. (this originates from the size of the town array in TTD
 static const int INDUSTRY_CUT_TREE_TICKS  = INDUSTRY_PRODUCE_TICKS * 2; ///< cycle duration for lumber mill's extra action
 
